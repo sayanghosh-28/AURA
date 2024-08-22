63bc026eb26ad1ab0b7e8475308efa8e8b138c14
sayanghosh-28 <babanghosh2801@gmail.com>
2024-08-22 21:46:39 +0530

    1st commit

---
+++ frontend/main.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function () {
+    $('.text').textillate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"bounceIn",
+        },
+        out:{
+            effect:"bounceOut",
+        },
+    });
+
+    //siri
+    
+    
+});
+
+var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    amplitude: 1,
+    speed: 0.30,
+    autostart: true
+});
